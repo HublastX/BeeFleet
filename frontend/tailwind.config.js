4c25c1f8cbe4f5033bd68149eadbc59ca7a9d523
Andra-sun <camileguimaraes463@gmail.com>
2025-03-17 11:21:16 -0300

    novo tom de roxo e ajuste para pegar temas padroes do tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,34 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: [],
+   content: ["./app/**/*.{js,ts,jsx,tsx}"],
    theme: {
-      colors: {
-         "bee-yellow": {
-            100: "#FFF4DC",
-            200: "#FFEAB8",
-            300: "#FFDF95",
-            400: "#FFD571",
-            500: "#FFCB4A",
-            600: "#FFC107",
-            700: "#DB9E00",
-         },
-         "bee-purple": {
-            100: "#EDE7F6",
-            200: "#C9B6E3",
-            300: "#AD92D4",
-            400: "#9170C6",
-            500: "#724FB7",
-            600: "#512DA8",
-         },
-         "bee-dark": {
-            100: "#F5F5F5",
-            300: "#BDBDBD",
-            500: "#424242",
-            600: "#2E2E2F",
-            700: "#212121",
+      extend: {
+         colors: {
+            "bee-yellow": {
+               100: "#FFF4DC",
+               200: "#FFEAB8",
+               300: "#FFDF95",
+               400: "#FFD571",
+               500: "#FFCB4A",
+               600: "#FFC107",
+               700: "#DB9E00",
+            },
+            "bee-purple": {
+               100: "#EDE7F6",
+               200: "#C9B6E3",
+               300: "#AD92D4",
+               400: "#9170C6",
+               500: "#724FB7",
+               600: "#512DA8",
+               700: "#3b2287",
+            },
+            "bee-dark": {
+               100: "#F5F5F5",
+               300: "#BDBDBD",
+               500: "#424242",
+               600: "#2E2E2F",
+               700: "#212121",
+            },
          },
       },
-      extend: {},
    },
    plugins: [],
 };
