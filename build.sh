ca8761317d0c8fde9569864b431c7f83e9437d54
wendellast <wendelalves01a@hotmail.com>
2025-03-19 22:40:22 -0300

    fix(build): fix erro do docker-compose

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+# Copia o arquivo .env
 cp .env backend/datahub/.env
+echo "Arquivo .env copiado."
 
-docker compose up --build
-
+# Tenta rodar docker compose up --build
+if ! docker compose up --build; then
+    echo "Erro ao executar 'docker compose up --build'. Tentando com 'docker-compose up --build'..."
+    docker-compose up --build
+fi
