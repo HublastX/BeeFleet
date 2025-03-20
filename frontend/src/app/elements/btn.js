1cb4bbb58a5442d6c7289bb8eeb22f147068d4e1
Andra-sun <camileguimaraes463@gmail.com>
2025-03-20 16:01:10 -0300

    feat(btn :lipstick:): arredondamento da borda alterada

--- frontend/src/app/elements/btn.js
+++ frontend/src/app/elements/btn.js
@@ -3,7 +3,7 @@ const Btn = ({ texto, onClick, className }) => {
    return (
       <button
          onClick={onClick}
-         className={`rounded bg-bee-purple-600 text-white px-6 py-3 font-semibold shadow-md hover:bg-bee-purple-700 transition-all duration-300 active:scale-95  ${className}`}
+         className={`rounded-xl bg-bee-purple-600 text-white px-6 py-3 font-semibold shadow-md hover:bg-bee-purple-700 transition-all duration-300 active:scale-95  ${className}`}
       >
          {texto}
       </button>
