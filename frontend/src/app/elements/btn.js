4d80e34f1b5b92f8d778f6bfa9b9d02699abbde4
Andra-sun <camileguimaraes463@gmail.com>
2025-03-21 16:33:10 -0300

    style(btn): arquivo organizado

--- frontend/src/app/elements/btn.js
+++ frontend/src/app/elements/btn.js
@@ -1,7 +1,7 @@
 const Btn = ({ texto, onClick, className, type }) => {
    return (
       <button
-      type={type}
+         type={type}
          onClick={onClick}
          className={`rounded-xl bg-bee-purple-600 text-white px-6 py-3 font-semibold shadow-md hover:bg-bee-purple-700 transition-all duration-300 active:scale-95  ${className}`}
       >
