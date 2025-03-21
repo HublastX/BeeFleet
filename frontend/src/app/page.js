59373f3d629aff5e211321b3959ef8478ba3bb55
Andra-sun <camileguimaraes463@gmail.com>
2025-03-21 16:26:01 -0300

    cleanup(page): modal antigo e header retirado do page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,30 +1,14 @@
 "use client";
-import { useState } from "react";
-import ModalDriver from "./components/Modal";
-import Header from "./components/Header";
-import Btn from "./elements/btn";
+import Modal from "./components/Modal";
 
 export default function Home() {
-   const [isModalOpen, setIsModalOpen] = useState(false);
-
-   const handleOpenModal = () => {
-      setIsModalOpen(true);
-   };
-
-   const handleCloseModal = () => {
-      setIsModalOpen(false);
-   };
-
    return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-         <Header onOpenModal={handleOpenModal} />
          <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
             <div className="flex gap-4 items-center flex-col sm:flex-row">
-               <Btn texto="Solicitar Carro" onClick={handleOpenModal} />
+               <Modal />
             </div>
          </main>
-
-         {isModalOpen && <ModalDriver onClose={handleCloseModal} />}
       </div>
    );
 }
