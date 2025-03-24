c765a11dcd5c2024102230a10da328d5b73536f6
Andra-sun <camileguimaraes463@gmail.com>
2025-03-24 16:35:53 -0300

    refactor(navbar): padding e tamanho do texto ajustado

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const NavBar = () => {
    return (
-      <aside className="mt-16 flex flex-col lg:mt-0 top-0 px-5  bg-white dark:bg-bee-dark-800 dark:border-bee-dark-400 text-bee-dark-600 h-screen transition-all duration-300 ease-in-out z-50 border-r border-bee-dark-100">
+      <aside className="mt-16 flex flex-col lg:mt-0 top-0 px-5  bg-bee-dark-100 dark:bg-bee-dark-800 dark:border-bee-dark-400 text-bee-dark-600 h-screen transition-all duration-300 ease-in-out z-50 border-r border-bee-dark-300">
          <div className="mx-auto flex justify-between items-center">
             <Link href="/" className="flex items-center gap-0 mt-1 p-2">
                <Image
