dae0909ff2929cc25a3e27c04dcf114cd7ef1618
Andra-sun <camileguimaraes463@gmail.com>
2025-03-25 17:13:55 -0300

    feat(header): nova div adicionada ao header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -15,14 +15,10 @@ const Header = () => {
       }
    };
    return (
-      <header className="flex w-full bg-bee-dark-100 border-bee-dark-300 z-99999 dark:border-bee-dark-400 dark:bg-bee-dark-800 lg:border-b">
+      <header className="sticky top-0 flex w-full bg-bee-dark-100 border-b border-bee-dark-300 z-99999 dark:border-bee-dark-400 dark:bg-bee-dark-800 lg:border-b">
          <div className="flex flex-col items-center justify-between grow lg:flex-row lg:px-6">
             <div className="flex items-center justify-between w-full gap-2 px-3 py-3 border-b border-bee-dark-100 dark:border-bee-dark-400 sm:gap-4 lg:justify-normal lg:border-b-0 lg:px-0 lg:py-4">
-               <Btn
-                  variant="secondary"
-                  type="button"
-                  onClick={handleToggle}
-               >
+               <Btn variant="secondary" type="button" onClick={handleToggle}>
                   {isMobileOpen ? (
                      <Icon name="xMark" className="h-7" />
                   ) : (
@@ -30,6 +26,8 @@ const Header = () => {
                   )}
                </Btn>
             </div>
+         </div>
+         <div className="flex items-center gap-2 2xsm:gap-3 pr-2">
             <Modal />
          </div>
       </header>
