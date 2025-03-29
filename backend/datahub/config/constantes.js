7ede8e8074c0dea71c7ea8ed69bb54b151f03cbf
wendellast <wendelalves01a@hotmail.com>
2025-03-29 15:26:45 -0300

    feat(config): mudança pra ts e tipagem

--- backend/datahub/config/constantes.js
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config({ path: "../../.env" });
-
-export const DATABASE_URL = process.env.DATABASE_URL || "";
-export const PORT_DATAHUB = process.env.PORT_DATAHUB || 3001;
-export const SECRET_KEY = process.env.JWT_SECRET || "chave-secreta";
---
+++ backend/datahub/config/constantes.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../../.env" });
+
+export const DATABASE_URL: string = process.env.DATABASE_URL || "";
+export const PORT_DATAHUB: number = parseInt(
+    process.env.PORT_DATAHUB || "3001",
+    10
+);
+export const SECRET_KEY: string = process.env.JWT_SECRET || "chave-secreta";
