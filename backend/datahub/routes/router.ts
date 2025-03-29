193fe7e2e8d8712e0434559caec151771b048e0f
wendellast <wendelalves01a@hotmail.com>
2025-03-29 15:34:49 -0300

    feat(router): cadastrando novas rotas que foram criadas

---
+++ backend/datahub/routes/router.ts
@@ -0,0 +1,19 @@
+import express, { Router } from "express";
+
+import loginManagerRoute from "./public/loginManagerRoute";
+import managerRoutes from "./private/managers/managersRoutes";
+import driverRoutes from "./private/drivers/driversRoutes";
+import eventRoutes from "./private/events/eventRoutes";
+import reportRoutes from "./private/reports/reportRoutes";
+import carRoutes from "./private/cars/carsRoutes";
+
+const router: Router = express.Router();
+
+router.use(loginManagerRoute);
+router.use(managerRoutes);
+router.use(driverRoutes);
+router.use(eventRoutes);
+router.use(reportRoutes);
+router.use(carRoutes);
+
+export default router;
