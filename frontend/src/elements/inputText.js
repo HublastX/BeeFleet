a728ab91f9fc66fe86c9b78258cf8d8778a35a1f
Andra-sun <camileguimaraes463@gmail.com>
2025-04-01 15:40:43 -0300

    refactor(input): cor de fundo do input trocada

--- frontend/src/elements/inputText.js
+++ frontend/src/elements/inputText.js
@@ -12,7 +12,7 @@ const InputText = ({ placeholder, className, name, type, variant, icon, value, o
          <input
             type={type}
             placeholder={placeholder}
-            className={`bg-white border rounded-lg outline-0 font-normal border-gray-300 text-bee-dark-600 text-sm block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder:text-gray-400 dark:text-white dark:focus:ring-bee-purple-400 dark:focus:border-bee-purple-400 ${
+            className={`bg-white border rounded-lg outline-0 font-normal border-gray-300 text-bee-dark-600 text-sm block w-full p-2.5 dark:bg-gray-800 dark:border-gray-500 dark:placeholder:text-gray-400 dark:text-white dark:focus:ring-bee-purple-400 dark:focus:border-bee-purple-400 ${
                variant === "withIcon" ? "pl-10" : ""
             } ${className}`}
             name={name}
