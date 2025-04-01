278d14f4b02d27885b0d96b4d604cd76557ec6c4
Andra-sun <camileguimaraes463@gmail.com>
2025-04-01 12:16:45 -0300

    refactor(header): novo modo do pegar dados do gestor

--- frontend/src/components/header/UserDropdown.js
+++ frontend/src/components/header/UserDropdown.js
@@ -1,30 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Dropdown } from "../../elements/ui/dropdown/Dropdown";
 import { DropdownItem } from "../../elements/ui/dropdown/DropdownItem";
-import Link from "next/link";
 import Icon from "@/elements/Icon";
 import Image from "next/image";
+import useAuth  from "@/hooks/useAuth";
 
 export default function UserDropdown() {
    const [isOpen, setIsOpen] = useState(false);
+   const { gestor, logout } = useAuth(); 
    function toggleDropdown(e) {
       e.stopPropagation();
       setIsOpen((prev) => !prev);
    }
+
    function closeDropdown() {
       setIsOpen(false);
    }
-   const [userName, setUserName] = useState("");
-   const [profilePhoto, setProfilePhoto] = useState("");
-   const [userEmail, setUserEmail] = useState("");
-   useEffect(() => {
-      const name = localStorage.getItem("name");
-      const photo = localStorage.getItem("photo");
-      const email = localStorage.getItem("email");
-      if (name) setUserName(name);
-      if (photo) setProfilePhoto(photo);
-      if (email) setUserEmail(email);
-   }, []);
+
+   if (!gestor) return null;
+
+   const { name, photo, email } = gestor;
+
    return (
       <div className="relative">
          <button
@@ -32,15 +28,15 @@ export default function UserDropdown() {
             className="flex items-center text-bee-dark-600 dark:text-bee-alert-500 dropdown-toggle"
          >
             <span className="mr-3 overflow-hidden rounded-full h-11 w-11">
-               {!profilePhoto ? (
-                  <Icon name="UserCircle"/>
+               {!photo ? (
+                  <Icon name="UserCircle" />
                ) : (
-                  <Image src={profilePhoto} alt="Profile" />
+                  <Image src={photo} alt="Profile" />
                )}
             </span>
 
             <span className="block mr-1 font-medium text-theme-sm">
-               {userName}
+               {name}
             </span>
 
             <Icon
@@ -55,11 +51,11 @@ export default function UserDropdown() {
          <Dropdown
             isOpen={isOpen}
             onClose={closeDropdown}
-            className="absolute right-0 mt-[17px] flex w-[260px] flex-col rounded-2xl border border-bee-dark-300 bg-white p-3 shadow-theme-lg dark:border-bee-dark-600 dark:bg-bee-dark-400"
+            className="absolute px-7 right-0 mt-[17px] flex  flex-col rounded-2xl border border-bee-dark-300 bg-white p-3 shadow-theme-lg dark:border-bee-dark-600 dark:bg-bee-dark-400"
          >
             <div className="dark:text-white text-bee-dark-600">
-               <span className="block font-medium">{userName}</span>
-               <span className="mt-0.5 block text-theme-xs">{userEmail}</span>
+               <span className="block font-medium">{name}</span>
+               <span className="mt-0.5 block text-theme-xs">{email}</span>
             </div>
             <ul className="flex flex-col gap-1 pt-4 pb-3 border-b border-bee-dark-300 dark:border-bee-dark-800">
                <li>
@@ -78,13 +74,13 @@ export default function UserDropdown() {
                   </DropdownItem>
                </li>
             </ul>
-            <Link
-               href="/singOut"
+            <button
+               onClick={() => logout()} 
                className="flex items-center gap-3 px-3 py-2 mt-3 font-medium text-bee-dark-600 rounded-lg group text-theme-sm hover:bg-bee-alert-600 dark:text-bee-alert-500 dark:hover:bg-bee-alert-600"
             >
                <Icon name="sair" className="w-6 h-6" strokeWidth={1.5} />
                Sair
-            </Link>
+            </button>
          </Dropdown>
       </div>
    );
