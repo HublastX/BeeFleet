c40e41e4b0ee46bf229fc188afa25ee1c45dc3dc
wendellast <wendelalves01a@hotmail.com>
2025-04-03 08:44:03 -0300

    feat(controoler): adicionado rotas de ver todos os carros

--- backend/datahub/controllers/drivers/getAllDriver.ts
+++ backend/datahub/controllers/drivers/getAllDriver.ts
@@ -1,7 +1,7 @@
 import { prisma } from "../../config/prisma";
 import { Request, Response } from "express";
 
-export const getAllDrivers = async (req: Request, res: Response) => {
+export const getDriver = async (req: Request, res: Response) => {
     try {
         const { id } = req.params;
         const driver = await prisma.driver.findUnique({ where: { id } });
@@ -11,5 +11,36 @@ export const getAllDrivers = async (req: Request, res: Response) => {
         res.json(driver);
     } catch (error) {
         res.status(500).json({ error: "Error fetching driver" });
+    }
+};
+
+export const getAllDrivers = async (
+    req: Request,
+    res: Response
+): Promise<Response> => {
+    try {
+        const drivers = await prisma.driver.findMany({
+            include: {
+                manager: {
+                    select: {
+                        id: true,
+                        name: true,
+                        email: true,
+                    },
+                },
+            },
+        });
+
+        return res.status(200).json({
+            success: true,
+            count: drivers.length,
+            data: drivers,
+        });
+    } catch (error) {
+        console.error("Error fetching drivers:", error);
+        return res.status(500).json({
+            success: false,
+            error: "Server Error",
+        });
     }
 };
