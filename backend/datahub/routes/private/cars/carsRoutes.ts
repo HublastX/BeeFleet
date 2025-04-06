f6495aea666f3ed5df45e278127e22c7d879241c
LuyysHenriqueKuromi <luizhenriquezfernandesmartinsz@gmail.com>
2025-04-05 21:06:02 -0300

    feat(controler e routes): criação da rota de ver car por id e ver todos os cars

--- backend/datahub/routes/private/cars/carsRoutes.ts
+++ backend/datahub/routes/private/cars/carsRoutes.ts
@@ -1,6 +1,8 @@
 import express, { Router } from "express";
-import { createCar } from "../../../controllers/cars/createCar";
 import { Request, Response, NextFunction } from "express";
+import { createCar } from "../../../controllers/cars/createCar";
+import { getCar } from "../../../controllers/cars/getAllCar"
+import { getAllCars } from "../../../controllers/cars/getAllCar"
 import { authenticateManager } from "../../../middlewares/auth";
 const carRoutes: Router = express.Router();
 
@@ -12,6 +14,26 @@ carRoutes.post(
         next: NextFunction
     ) => void,
     createCar
+);
+
+carRoutes.get(
+    "/cars/:id",
+    authenticateManager as (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => void,
+    getCar as (req: Request, res: Response) => void
+);
+
+carRoutes.get(
+    "/cars",
+    authenticateManager as (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => void,
+    getAllCars as (req: Request, res: Response) => void
 );
 
 export default carRoutes;
