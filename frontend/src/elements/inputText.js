41c33d20c27bc3b7605521a5975efa946a89fae8
Andra-sun <camileguimaraes463@gmail.com>
2025-04-08 18:37:44 -0300

    feat(input): auto fill do input alterado

--- frontend/src/elements/inputText.js
+++ frontend/src/elements/inputText.js
@@ -6,13 +6,13 @@ const InputText = ({ placeholder, className, name, type, variant, icon, value, o
       <div className="relative">
          {variant === "withIcon" && icon && (
             <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-               <Icon name={icon} strokeWidth={strokeWidth} className="w-4 h-4 text-gray-900 dark:text-white" />
+               <Icon name={icon} strokeWidth={strokeWidth} className="w-4 h-4 text-gray-900 dark:text-white " />
             </span>
          )}
          <input
             type={type}
             placeholder={placeholder}
-            className={`bg-white border rounded-lg outline-0 font-normal border-gray-300 text-bee-dark-600 text-sm block w-full p-2.5 dark:bg-gray-800 dark:border-gray-500 dark:placeholder:text-gray-400 dark:text-white dark:focus:ring-bee-purple-400 dark:focus:border-bee-purple-400 ${
+            className={`bg-white border rounded-lg outline-0 font-normal border-gray-300 text-bee-dark-600 text-md block w-full p-2.5 dark:bg-gray-800 dark:border-gray-500 dark:placeholder:text-gray-400 dark:text-white dark:focus:ring-bee-purple-400 dark:focus:border-bee-purple-400 autofill:bg-white dark:autofill:bg-bee-dark-800 dark:autofill:text-white ${
                variant === "withIcon" ? "pl-10" : ""
             } ${className}`}
             name={name}
