fb331fb5f98412ac31b0890159be526ea3d241d6
Marcos matos <marcosjk183@gmail.com>
2025-04-08 18:22:21 -0300

    feat(home): reorganizando o blockdamain

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,29 +2,24 @@
 import Btn from "@/elements/btn";
 import Link from "next/link";
 import useAuth from "@/hooks/useAuth";
-import { useState } from "react";
 import LoginOverlay from "@/components/blockdamain";
 
 export default function Home() {
    const { gestor } = useAuth();
-   const [isLoggedIn, setIsLoggedIn] = useState(!!gestor);
 
    return (
       <div className="grid grid-cols-12 gap-4 md:gap-6">
 
-         {!isLoggedIn && <LoginOverlay onLogin={() => setIsLoggedIn(true)} />}
-
          <div className="col-span-12 xl:col-span-5">
             {!gestor ? (
-               <Link href="/login">
-                  <Btn variant="secondary" texto="Login" />
-               </Link>
+               <LoginOverlay />
             ) : (
                <div>
                   <p>Bem-vindo, {gestor.name}!</p>
                </div>
             )}
          </div>
+
       </div>
    );
 }
