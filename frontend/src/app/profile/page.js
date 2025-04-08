482a2e07cb9fe537c63a13e21dc524415cf2e398
Andra-sun <camileguimaraes463@gmail.com>
2025-04-08 20:04:49 -0300

    feat(profile): tela de perfil do gestor criada

---
+++ frontend/src/app/profile/page.js
@@ -0,0 +1,66 @@
+"use client";
+import useAuth from "@/hooks/useAuth";
+import Icon from "@/elements/Icon";
+import Image from "next/image";
+import Link from "next/link";
+import Btn from "@/elements/btn";
+
+function Manager() {
+  const { gestor } = useAuth();
+  if (!gestor) return null;
+
+  const { name, photo, email } = gestor;
+
+  return (
+    <div className="p-4 space-y-6">
+      {/* Perfil */}
+      <div className="flex items-center gap-6 bg-white dark:bg-bee-dark-400 p-6 rounded-lg shadow-theme-lg">
+        <div className="flex-shrink-0">
+          {!photo ? (
+            <Icon name="UserCircle" className="w-24 h-24 text-gray-400" />
+          ) : (
+            <Image
+              src={photo}
+              alt={`Foto de perfil de ${name}`}
+              width={96}
+              height={96}
+              className="rounded-full object-cover w-24 h-24"
+            />
+          )}
+        </div>
+
+        <div className="flex-1">
+          <h1 className="text-2xl font-black capitalize">{name}</h1>
+          <p className="text-sm text-gray-600 dark:text-gray-300">{email}</p>
+        </div>
+
+        <Link
+          href="profile/edit"
+        >
+          <Btn texto="Editar perfil" />
+        </Link>
+      </div>
+
+      <hr className="py-4" />
+
+      {/* Estatísticas */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <div className="p-6 bg-white dark:bg-bee-dark-400 rounded-lg shadow-theme-lg">
+          <h2 className="text-lg font-semibold mb-2">Total de motoristas adicionados</h2>
+          <p className="text-4xl font-black text-bee-yellow-700">100</p>
+        </div>
+
+        <div className="p-6 bg-white dark:bg-bee-dark-400 rounded-lg shadow-theme-lg">
+          <h2 className="text-lg font-semibold mb-2">Total de carros adicionados</h2>
+          <p className="text-4xl font-black text-bee-yellow-700">50</p>
+        </div>
+        <div className="p-6 bg-white dark:bg-bee-dark-400 rounded-lg shadow-theme-lg">
+          <h2 className="text-lg font-semibold mb-2">Total de eventos finalizados</h2>
+          <p className="text-4xl font-black text-bee-yellow-700">60</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Manager;
