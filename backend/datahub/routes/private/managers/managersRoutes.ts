305ed44399323c724f197b77d7b50f85a32f4f22
LuyysHenriqueKuromi <luizhenriquezfernandesmartinsz@gmail.com>
2025-04-10 17:55:17 -0300

    refactor(routes): criação da rota put managers

--- backend/datahub/routes/private/managers/managersRoutes.ts
+++ backend/datahub/routes/private/managers/managersRoutes.ts
@@ -2,6 +2,7 @@ import express, { Router } from "express";
 import { createManager } from "../../../controllers/managers/createManager";
 import { getManager } from "../../../controllers/managers/getAllManager";
 import { getAllManagers } from "../../../controllers/managers/getAllManager";
+import { putManager } from "../../../controllers/managers/putManager";
 import { deleteManager } from "../../../controllers/managers/deleteManager";
 
 const managerRoutes: Router = express.Router();
@@ -28,7 +29,15 @@ managerRoutes.get("/managers", async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-})
+});
+
+managerRoutes.put("/managers/:id", async (req, res, next) => {
+    try {
+        await putManager(req, res);
+    } catch (error) {
+        next(error);
+    }
+});
 
 managerRoutes.delete("/managers/:id", async (req, res, next) => {
     try {
