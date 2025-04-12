a93166c346914ba3e16bb228b9877c0fadcd854b
Andra-sun <camileguimaraes463@gmail.com>
2025-04-12 19:26:16 -0300

    feat(pagination): cores da paginacao ajustada

--- frontend/src/components/table/Pagination.js
+++ frontend/src/components/table/Pagination.js
@@ -20,15 +20,15 @@ const Pagination = ({
    const pagesAroundCurrent = getPages(currentPage, totalPages);
 
    return (
-      <div className="flex items-center justify-between w-full">
-         <span className="text-sm text-gray-500 dark:text-gray-400 ml-4">
+      <div className="flex items-center justify-center w-full md:justify-between">
+         <span className="text-sm text-gray-500 dark:text-gray-400 ml-4 hidden md:block">
             Total de motoristas: {totalMotoristas}
          </span>
          <div className="flex items-center">
             <button
                onClick={() => onPageChange(currentPage - 1)}
                disabled={currentPage === 1}
-               className="mr-2.5 flex items-center h-10 justify-center rounded-lg border border-gray-300 bg-white px-3.5 py-2.5 text-gray-700 shadow-theme-xs hover:bg-gray-50 disabled:opacity-50 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] text-sm"
+               className="mr-2.5 flex items-center h-10 justify-center rounded-lg border border-gray-300 bg-white px-3.5 sm:px-3 py-2.5 text-gray-700 shadow-theme-xs hover:bg-gray-50 disabled:opacity-50 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] text-sm"
             >
                <Icon name="sEsquerda" strokeWidth={2} className="w-5 h-5" />
             </button>
@@ -38,7 +38,7 @@ const Pagination = ({
                   <>
                      <button
                         onClick={() => onPageChange(1)}
-                        className="text-gray-700 dark:text-gray-400 hover:text-brand-500 text-sm"
+                        className="text-bee-dark-600 dark:text-bee-alert-500 hover:text-brand-500 text-sm"
                      >
                         1
                      </button>
@@ -54,8 +54,8 @@ const Pagination = ({
                      onClick={() => onPageChange(page)}
                      className={`px-4 py-2 rounded ${
                         currentPage === page
-                           ? "bg-brand-500 text-white"
-                           : "text-gray-700 dark:text-gray-400"
+                           ? "bg-brand-500 text-gray-500 "
+                           : "text-bee-dark-600 dark:text-bee-alert-500"
                      } flex w-10 items-center justify-center h-10 rounded-lg text-sm font-medium hover:bg-blue-500/[0.08] hover:text-brand-500 dark:hover:text-brand-500`}
                   >
                      {page}
@@ -69,7 +69,7 @@ const Pagination = ({
                      )}
                      <button
                         onClick={() => onPageChange(totalPages)}
-                        className="text-gray-700 dark:text-gray-400 hover:text-brand-500 text-sm"
+                        className="text-bee-dark-600 dark:text-bee-alert-500 hover:text-brand-500 text-sm"
                      >
                         {totalPages}
                      </button>
@@ -80,7 +80,7 @@ const Pagination = ({
             <button
                onClick={() => onPageChange(currentPage + 1)}
                disabled={currentPage === totalPages}
-               className="ml-2.5 flex items-center justify-center rounded-lg border border-gray-300 bg-white px-3.5 py-2.5 text-gray-700 shadow-theme-xs text-sm hover:bg-gray-50 h-10 disabled:opacity-50 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03]"
+               className="ml-2.5 flex items-center justify-center rounded-lg border border-gray-300 bg-white px-3.5 sm:px-3 py-2.5 text-gray-700 shadow-theme-xs text-sm hover:bg-gray-50 h-10 disabled:opacity-50 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03]"
             >
                <Icon name="sDireita" strokeWidth={2} className="w-5 h-5" />
             </button>
