8f8606b737ab59804fbe2d65b21ea0886a437372
Andra-sun <camileguimaraes463@gmail.com>
2025-04-12 20:20:44 -0300

    test(cars): teste para ver o funcionamento do get cars

--- frontend/src/app/cars/page.js
+++ frontend/src/app/cars/page.js
@@ -2,11 +2,13 @@
 import withAuth from "@/utils/withAuth";
 import Link from "next/link";
 import Btn from "@/elements/btn";
+import useCar from "@/hooks/useCar";
 function Cars() {
+   const { carro, carregando, erro } = useCar();
    return (
       <div>
          <div className="p-2 mb-3">
-            <Link href="/createCar">
+            <Link href="/cars/create">
                <Btn
                   variant="primary"
                   texto="Adicionar Carro"
@@ -14,6 +16,18 @@ function Cars() {
                />
             </Link>
          </div>
+         {/* teste pra ve se a rota esta funcionando */}
+         {carro.map((car) => (
+            <div
+               key={car.id}
+               className="bg-gray-800 text-white p-4 rounded-lg mb-4 shadow-md"
+            >
+               <h2 className="text-xl font-bold">{car.model}</h2>
+               <p>Placa: {car.plate}</p>
+               <p>Ano: {car.year}</p>
+               <p>Cor: {car.color}</p>
+            </div>
+         ))}
       </div>
    );
 }
