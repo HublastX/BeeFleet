cee802b8281f70f489f987bdc9f314da80f60953
wendellast <wendelalves01a@hotmail.com>
2025-04-13 14:34:36 -0300

    build(models,libs): libs e nova models para suporte a imagens

---
+++ backend/datahub/prisma/migrations/20250413160512_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Manager` ADD COLUMN `image` VARCHAR(191) NULL;
---
+++ backend/datahub/prisma/migrations/20250413161656_/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `Car` ADD COLUMN `image` VARCHAR(191) NULL;
+
+-- AlterTable
+ALTER TABLE `Driver` ADD COLUMN `image` VARCHAR(191) NULL;
