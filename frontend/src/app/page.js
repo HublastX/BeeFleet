8f4101c60461fa3be7682721584cc995202f28d9
Andra-sun <camileguimaraes463@gmail.com>
2025-04-14 15:42:12 -0300

    test(home): teste para ver como os dados estao sendo salvos

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,13 @@
-"use client"
+"use client";
 import Btn from "@/elements/btn";
 import Link from "next/link";
 import useAuth from "@/hooks/useAuth";
+import Image from "next/image";
+import withAuth from "@/utils/withAuth";
 
-export default function Home() {
-   const { gestor } = useAuth();
+function Home() {
+   const { gestor, erro, carregando } = useAuth();
+   console.log("Imagem:", gestor?.imagem);
 
    return (
       <div className="flex items-center justify-center h-[80vh] w-full">
@@ -13,16 +16,45 @@ export default function Home() {
                <p className="text-xl font-bold text-red-500 flex items-center gap-2">
                   <span>⚠️</span> Acesso Restrito
                </p>
-               <p className="text-gray-600 dark:text-gray-100 text-center">Para acessar, realize o login!</p>
+               <p className="text-gray-600 dark:text-gray-100 text-center">
+                  Para acessar, realize o login!
+               </p>
                <Link href="/login">
                   <Btn>Fazer Login</Btn>
                </Link>
             </div>
          ) : (
             <div>
-               <p>Bem-vindo, {gestor.name}!</p>
+               <h1>Perfil</h1>
+
+               {carregando && <p>Carregando...</p>}
+               {erro && <p>{erro}</p>}
+
+               <div>
+                  <p>
+                     <strong>Nome:</strong> {gestor.name}
+                  </p>
+                  <p>
+                     <strong>Email:</strong> {gestor.email}
+                  </p>
+                  <p>
+                     <strong>Imagem:</strong> {gestor.image}
+                  </p>
+               </div>
+
+               {gestor.image && (
+                  <Image
+                     src={`${process.env.NEXT_PUBLIC_API_URL}${gestor.image}`}
+                     alt="Imagem de Perfil"
+                     width={100}
+                     height={100}
+                     className="rounded-full"
+                     unoptimized
+                  />
+               )}
             </div>
          )}
       </div>
    );
 }
+export default withAuth(Home);
