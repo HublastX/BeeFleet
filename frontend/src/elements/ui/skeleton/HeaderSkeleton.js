f69fef81de30ca40785ddecbf23d7399e42e0b51
Andra-sun <camileguimaraes463@gmail.com>
2025-04-15 17:44:01 -0300

    feat(skeleton): pasta para os esqueletos montada

---
+++ frontend/src/elements/ui/skeleton/HeaderSkeleton.js
@@ -0,0 +1,8 @@
+const HeaderSkeleton = () => {
+    return (
+       <div className="w-40 h-8 rounded-xl bg-gray-300 dark:bg-bee-dark-500 animate-pulse" />
+    );
+ };
+ 
+ export default HeaderSkeleton;
+ 
