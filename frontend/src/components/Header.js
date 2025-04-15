049ab82aa465e50c1b24a78300e1a3ba73dd0917
Andra-sun <camileguimaraes463@gmail.com>
2025-04-15 17:49:23 -0300

    feat(header): skeleton adicionado ao header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavBar } from "@/components/navbar/navBarContext";
 import useAuth from "@/hooks/useAuth";
 import UserDropdown from "./header/UserDropdown";
 import Btn from "@/elements/btn";
 import Icon from "@/elements/Icon";
 import Link from "next/link";
+import HeaderSkeleton from "@/elements/ui/skeleton/HeaderSkeleton";
 
 const Header = () => {
    const { isMobileOpen, toggleNavBar, toggleMobileNavBar } = useNavBar();
@@ -16,13 +17,18 @@ const Header = () => {
       }
    };
 
-   const { gestor } = useAuth();
+   const { gestor, erro, carregando } = useAuth();
 
    return (
       <header className="sticky top-0 flex w-full z-40 bg-bee-dark-100 border-b border-bee-dark-300 dark:border-bee-dark-400 dark:bg-bee-dark-800 lg:border-b">
          <div className="flex items-center justify-between grow flex-row px-6">
             <div className="flex items-center gap-2  border-bee-dark-100 dark:border-bee-dark-400 justify-between border-b-0 px-0 py-4">
-               <Btn variant="secondary" type="button" onClick={handleToggle} className={`${!gestor ? "cursor-not-allowed" : ""}`}>
+               <Btn
+                  variant="secondary"
+                  type="button"
+                  onClick={handleToggle}
+                  className={`${!gestor ? "cursor-not-allowed" : ""}`}
+               >
                   {isMobileOpen ? (
                      <Icon name="xMark" className="h-7" />
                   ) : (
@@ -31,22 +37,26 @@ const Header = () => {
                </Btn>
             </div>
 
-            <div className="flex items-center justify-between gap- py-4justify-end px-0">
-               {gestor ? (
-                  <>
-                     <div className="flex items-center gap-2 2xsm:gap-3">
-                     </div>
-                     <UserDropdown />
-                  </>
+            <div className="flex items-center justify-end gap-2 py-4 px-0">
+            {carregando ? (
+                  <HeaderSkeleton />
                ) : (
-                  <div className="flex gap-2">
-                     <Link href="/login">
-                        <Btn variant="primary" texto="Login" />
-                     </Link>
-                     <Link href="/register">
-                        <Btn variant="outline" texto="Registre-se" />
-                     </Link>
-                  </div>
+                  <>
+                     {gestor ? (
+                        <>
+                           <UserDropdown />
+                        </>
+                     ) : (
+                        <div className="flex gap-2">
+                           <Link href="/login">
+                              <Btn variant="primary" texto="Login" />
+                           </Link>
+                           <Link href="/register">
+                              <Btn variant="outline" texto="Registre-se" />
+                           </Link>
+                        </div>
+                     )}
+                  </>
                )}
             </div>
          </div>
