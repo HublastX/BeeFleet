7b12e6b410002c19229918a67248060ea5db53b6
LuyysHenriqueKuromi <luizhenriquezfernandesmartinsz@gmail.com>
2025-04-16 20:53:06 -0300

    feat(controllers): criação do controller get event

---
+++ backend/datahub/controllers/events/getAllEvent.ts
@@ -0,0 +1,15 @@
+import { prisma } from "../../config/prisma";
+import { Request, Response } from "express";
+
+export const getEvent = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const event = await prisma.event.findUnique({ where: { id } });
+        if (!event) {
+            return res.status(404).json({ error: "Evento não encontrado" });
+        }
+        res.json(event);
+    } catch (error) {
+        res.status(500).json({ error: "Erro ao buscar evento" });
+    }
+}
