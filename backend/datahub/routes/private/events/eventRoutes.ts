4b537f09a1b6fb94dc6ea22f20faf24701295fd4
LuyysHenriqueKuromi <luizhenriquezfernandesmartinsz@gmail.com>
2025-04-16 21:40:16 -0300

    refactor(routes): criação da rota getall events

--- backend/datahub/routes/private/events/eventRoutes.ts
+++ backend/datahub/routes/private/events/eventRoutes.ts
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import { createEvent } from "../../../controllers/events/createEvent";
 import { getEvent } from "../../../controllers/events/getAllEvent";
+import { getAllEvents } from "../../../controllers/events/getAllEvent";
 import { Request, Response, NextFunction } from "express";
 import { authenticateManager } from "../../../middlewares/auth";
 const eventRoutes: Router = express.Router();
@@ -25,6 +26,16 @@ eventRoutes.get(
         next: NextFunction
     ) => void,
     getEvent as (req: Request, res: Response) => void
+);
+
+eventRoutes.get(
+    "/events/checkout",
+    authenticateManager as (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => void,
+    getAllEvents as (req: Request, res: Response) => void
 );
 
 export default eventRoutes;
