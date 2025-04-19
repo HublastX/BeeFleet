b930db7d8c9bc9de3e1c47a3777df4f3c8b11a7e
Andra-sun <camileguimaraes463@gmail.com>
2025-04-19 16:24:21 -0300

    refactor(elements): cores dos elementos ajustados

--- frontend/src/elements/card.js
+++ frontend/src/elements/card.js
@@ -5,7 +5,7 @@ const Card = ({ icone, titulo, quantidade, estado }) => {
       <div className="p-5 rounded-2xl border border-bee-dark-300 bg-bee-dark-100 dark:border-bee-dark-400 dark:bg-bee-dark-800">
          <div className="flex h-12 w-12 items-center justify-center rounded-xl bg-gray-100 dark:bg-gray-800">
             <Icon
-               className="text-bee-dark-600 size-6 dark:text-white/90"
+               className="text-bee-dark-600 size-6 dark:text-bee-alert-500"
                name={icone}
             />
          </div>
@@ -20,13 +20,13 @@ const Card = ({ icone, titulo, quantidade, estado }) => {
                </h4>
             </div>
             {estado === "aumento" && (
-               <Badge color="success">
+               <Badge color="success" size="md">
                   <Icon name="sCima" className="size-3" strokeWidth={3} />
                   9.05%
                </Badge>
             )}
             {estado === "baixa" && (
-               <Badge color="error">
+               <Badge color="error" size="md"  >
                   <Icon name="sBaixo" className="size-3" strokeWidth={3} />
                   9.05%
                </Badge>
