0a82c2af5b4306ce1281faa5983523bd0e906952
Marcos matos <marcosjk183@gmail.com>
2025-04-24 12:16:55 -0300

    feat(home): modifica o componente no Toast

--- frontend/src/elements/Toast.js
+++ frontend/src/elements/Toast.js
@@ -1,23 +1,39 @@
 import Icon from "./Icon";
-export default function Toast({ message, description, type = "success", onClose }) {
+export default function Toast({
+   message,
+   description,
+   type = "success",
+   onClose,
+}) {
    const colors = {
-      success: "border-green-500",
-      error: "border-red-500",
-      warning: "border-orange-400",
+      success: "border-green-500 text-green-500",
+      error: "border-red-500 text-red-500",
+      warning: "border-orange-400 text-orange-400",
+   };
+   const icon = {
+      success: "check",
+      error: "error",
+      warning: "warning",
    };
 
    return (
       <div className="fixed top-5 right-5 z-50">
          <div
-            className={`px-4 py-3 border-2 bg-white dark:bg-bee-dark-400 rounded-lg shadow-lg transition-all duration-300 ${colors[type]}`}
+            className={`px-4 py-3 border-2 rounded-lg shadow-lg transition-all duration-300 bg-bee-dark-100 dark:bg-bee-dark-800 ${colors[type]}`}
          >
             <div className="flex items-center justify-between gap-3">
+               <Icon name={icon[type]} className={`size-7 ${colors[type]}`} strokeWidth={3} />
                <div className="flex flex-col">
-                  <span className="font-semibold text-2xl">{message}</span>
-                  {description && <span>{description}</span>}
+                  <span className="font-black text-2xl">{message}</span>
+                  {description && (
+                     <span className="font-semibold ">{description}</span>
+                  )}
                </div>
-               <button onClick={onClose ?? (() => {})} className="font-bold ml-2 text-lg leading-none">
-                  <Icon name="xMark" className="size-6 hover:text-gray-400"/>
+               <button
+                  onClick={onClose ?? (() => {})}
+                  className="font-bold ml-2 text-lg leading-none"
+               >
+                  <Icon name="xMark" className="size-6 hover:text-gray-400" />
                </button>
             </div>
          </div>
