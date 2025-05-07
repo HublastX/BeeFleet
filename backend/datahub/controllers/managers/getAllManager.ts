dada3162ba4aa573bc01faa5c6a2b8b79c35c2b3
LuyysHenriqueKuromi <luizhenriquezfernandesmartinsz@gmail.com>
2025-05-07 12:45:15 -0300

    feat(controllers): criado arquivo seperado getmanager do getallmanager

--- backend/datahub/controllers/managers/getAllManager.ts
+++ backend/datahub/controllers/managers/getAllManager.ts
@@ -1,19 +1,6 @@
 import { prisma } from "../../config/prisma";
 import { Request, Response } from "express";
 
-export const getManager = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const manager = await prisma.manager.findUnique({ where: { id } });
-        if (!manager) {
-            return res.status(404).json({ error: "Manager not found"});
-        }
-        res.json(manager);
-    } catch (error) {
-        res.status(500).json({ error: "Error fetching manager"});
-    }
-};
-
 export const getAllManagers = async (
     req: Request,
     res: Response
