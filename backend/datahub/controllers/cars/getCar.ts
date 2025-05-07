5292d5c4305692962e27e32986c5f2ece65059c7
LuyysHenriqueKuromi <luizhenriquezfernandesmartinsz@gmail.com>
2025-05-07 12:14:05 -0300

    feat(controllers): criado arquivo seperado getcar do getallcar

---
+++ backend/datahub/controllers/cars/getCar.ts
@@ -0,0 +1,15 @@
+import { prisma } from "../../config/prisma";
+import { Request, Response } from "express";
+
+export const getCar = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const car = await prisma.car.findUnique({ where: { id}});
+        if (!car) {
+            return res.status(404).json({ error: "Car not found" });
+        }
+        res.json(car);
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching car" });
+    }
+};
