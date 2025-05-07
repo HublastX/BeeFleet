4e9026e459d6c5824981b316841002d0eefa6fa0
Andra-sun <camileguimaraes463@gmail.com>
2025-05-07 16:12:37 -0300

    feat(event): radio adicionado para escolher entre saida e chegada

--- frontend/src/app/event/page.js
+++ frontend/src/app/event/page.js
@@ -1,24 +1,52 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import withAuth from "@/utils/withAuth";
 import Saida from "@/components/eventForm/saida";
 
 function Events() {
+   const [tipoEvento, setTipoEvento] = useState("");
+
    return (
       <div>
          <div className="flex justify-between border-b items-center border-bee-dark-300 dark:border-bee-dark-400 pb-3">
-            <h2 className="text-3xl font-bold mb-6 ">
-               Gerenciar Evento
-            </h2>
-            <select
-               name="eventType"
-               className="border h-fit border-bee-dark-300 dark:border-bee-dark-400 py-3 px-4 rounded dark:bg-bee-dark-800 bg-bee-dark-100"
-            >
-               <option value="1">Chegada</option>
-               <option value="2">Saída</option>
-            </select>
+            <h2 className="text-3xl font-bold mb-6">Gerenciar Evento</h2>
+            <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-5">
+               <h1 className="text-base font-medium text-bee-dark-700 dark:text-white">
+                  Tipo de evento:
+               </h1>
+
+               <label className="flex items-center gap-2 cursor-pointer text-sm text-bee-dark-700 dark:text-white">
+                  <input
+                     type="radio"
+                     name="tipoEvento"
+                     value="chegada"
+                     checked={tipoEvento === "chegada"}
+                     onChange={(e) => setTipoEvento(e.target.value)}
+                     className="accent-bee-primary-500"
+                  />
+                  Chegada
+               </label>
+
+               <label className="flex items-center gap-2 cursor-pointer text-sm text-bee-dark-700 dark:text-white">
+                  <input
+                     type="radio"
+                     name="tipoEvento"
+                     value="saida"
+                     checked={tipoEvento === "saida"}
+                     onChange={(e) => setTipoEvento(e.target.value)}
+                     className="accent-bee-primary-500"
+                  />
+                  Saída
+               </label>
+            </div>
          </div>
-         <Saida />
+         {!tipoEvento && (
+            <div className="mt-4 text-bee-dark-600 dark:text-bee-light-200">
+               aqui vem uma pequena explicacao de como funciona um evento
+            </div>
+         )}
+
+         {tipoEvento === "saida" && <Saida />}
       </div>
    );
 }
