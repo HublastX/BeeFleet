3bad8d4d9e874a5bb2f021709e2647604683c722
wendellast <wendelalves01a@hotmail.com>
2025-05-10 16:20:45 -0300

    chore(datahub): atualiza porta e caminho do .env

--- backend/datahub/config/constantes.ts
+++ backend/datahub/config/constantes.ts
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
 
-dotenv.config({ path: "../../.env" });
+dotenv.config({ path: ".env" });
 
 export const DATABASE_URL: string = process.env.DATABASE_URL || "";
 export const PORT_DATAHUB: number = parseInt(
-    process.env.PORT_DATAHUB || "3001",
+    process.env.PORT_DATAHUB || "5004",
     10
 );
 export const SECRET_KEY: string = process.env.JWT_SECRET || "chave-secreta";
