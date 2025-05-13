eba25f487f7ea1c7219dae2a983dcc8bd44d0d34
Andra-sun <camileguimaraes463@gmail.com>
2025-05-13 16:19:53 -0300

    feat(detailtable): tabelas detail funcionando

--- frontend/src/components/table/detailDriverTable.js
+++ frontend/src/components/table/detailDriverTable.js
@@ -7,24 +7,70 @@ import {
    TableHeader,
    TableRow,
 } from "../../elements/ui/table";
+import useEvents from "@/hooks/useEvent";
+import { useParams } from "next/navigation";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import useAuth from "@/hooks/useAuth";
+import useCar from "@/hooks/useCar";
 
 export default function DetailDriverTable() {
+   const { events } = useEvents();
+   const { id } = useParams();
+   const { carro } = useCar();
+   const { gestores } = useAuth();
+
+   const driverEvents = events
+      .filter((event) => event.driverId === id)
+      .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+   const formatDate = (dateString) => {
+      const date = new Date(dateString);
+      return format(date, "dd/MM/yyyy, HH'h'mm", { locale: ptBR });
+   };
+
+   const getManagerName = (managerId) => {
+      const manager = gestores.find((g) => g.id === managerId);
+      return manager ? manager.name : "Gestor não encontrado";
+   };
+
+   const getCarInfo = (carId) => {
+      const car = carro.find((c) => c.id === carId);
+      return car ? `${car.model} - ${car.plate}` : "Carro não encontrado";
+   };
+
+   const translateStatus = (eventType) => {
+      return eventType === "CHECKOUT" ? "Saída" : "Entrada";
+   };
+
    return (
       <div className="hidden md:flex overflow-x-auto rounded-xl border border-bee-dark-300 bg-bee-dark-100 dark:border-bee-dark-400 dark:bg-bee-dark-800">
          <Table className="min-w-[500px] text-sm sm:text-base">
             {/* Table Header */}
             <TableHeader className="border-b border-bee-dark-300 dark:border-bee-dark-400 text-bee-dark-600 dark:text-bee-alert-500">
                <TableRow>
-                  <TableCell isHeader className="px-3 py-2 sm:px-5 sm:py-3 text-start whitespace-nowrap">
+                  <TableCell
+                     isHeader
+                     className="px-3 py-2 sm:px-5 sm:py-3 text-start whitespace-nowrap"
+                  >
                      Tipo
                   </TableCell>
-                  <TableCell isHeader className="px-3 py-2 sm:px-5 sm:py-3 text-start whitespace-nowrap">
+                  <TableCell
+                     isHeader
+                     className="px-3 py-2 sm:px-5 sm:py-3 text-start whitespace-nowrap"
+                  >
                      Carro
                   </TableCell>
-                  <TableCell isHeader className="px-3 py-2 sm:px-5 sm:py-3 text-start whitespace-nowrap">
+                  <TableCell
+                     isHeader
+                     className="px-3 py-2 sm:px-5 sm:py-3 text-start whitespace-nowrap"
+                  >
                      Data
                   </TableCell>
-                  <TableCell isHeader className="px-3 py-2 sm:px-5 sm:py-3 text-start whitespace-nowrap">
+                  <TableCell
+                     isHeader
+                     className="px-3 py-2 sm:px-5 sm:py-3 text-start whitespace-nowrap"
+                  >
                      Registrado por
                   </TableCell>
                </TableRow>
@@ -32,20 +78,27 @@ export default function DetailDriverTable() {
 
             {/* Table Body */}
             <TableBody className="divide-y divide-bee-dark-300 dark:divide-bee-dark-400">
-               <TableRow className="dark:hover:bg-bee-alert-600 hover:bg-bee-alert-500">
-                  <TableCell className="px-3 py-2 sm:px-4 sm:py-3 text-bee-dark-600 text-start dark:text-bee-alert-500 whitespace-nowrap">
-                     Saída
-                  </TableCell>
-                  <TableCell className="px-3 py-2 sm:px-4 sm:py-3 text-bee-dark-600 text-start dark:text-bee-alert-500 whitespace-nowrap">
-                     HB20 - ABC1234
-                  </TableCell>
-                  <TableCell className="px-3 py-2 sm:px-4 sm:py-3 text-bee-dark-600 text-start dark:text-bee-alert-500 whitespace-nowrap">
-                     03/05/2025, 14h
-                  </TableCell>
-                  <TableCell className="px-3 py-2 sm:px-4 sm:py-3 text-bee-dark-600 text-start dark:text-bee-alert-500 whitespace-nowrap">
-                     Davi Brito
-                  </TableCell>
-               </TableRow>
+               {driverEvents.map((event) => (
+                  <TableRow
+                     key={event.id}
+                     className="dark:hover:bg-bee-alert-600 hover:bg-bee-alert-500"
+                  >
+                     <TableCell
+                        className={`px-3 py-2 sm:px-4 sm:py-3 text-start dark:text-bee-alert-500 whitespace-nowrap ${translateStatus(event.eventType) === "Entrada" ? "text-bee-alert-100" : "text-bee-alert-300"}`}
+                     >
+                        {translateStatus(event.eventType)}
+                     </TableCell>
+                     <TableCell className="px-3 py-2 sm:px-4 sm:py-3 text-bee-dark-600 text-start dark:text-bee-alert-500 whitespace-nowrap">
+                        {getCarInfo(event.carId)}
+                     </TableCell>
+                     <TableCell className="px-3 py-2 sm:px-4 sm:py-3 text-bee-dark-600 text-start dark:text-bee-alert-500 whitespace-nowrap">
+                        {formatDate(event.createdAt)}
+                     </TableCell>
+                     <TableCell className="px-3 py-2 sm:px-4 sm:py-3 text-bee-dark-600 text-start dark:text-bee-alert-500 whitespace-nowrap">
+                        {getManagerName(event.managerId)}
+                     </TableCell>
+                  </TableRow>
+               ))}
             </TableBody>
          </Table>
       </div>
