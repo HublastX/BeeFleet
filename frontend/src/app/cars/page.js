4abcfa5555b1fedebc97cdb2b6963edce9736883
Andra-sun <camileguimaraes463@gmail.com>
2025-05-23 20:50:17 -0300

    feat(cars): adiciona modal de criação de carros

--- frontend/src/app/cars/page.js
+++ frontend/src/app/cars/page.js
@@ -1,29 +1,29 @@
 "use client";
 import React, { useState } from "react";
 import withAuth from "@/utils/withAuth";
-import Link from "next/link";
 import Btn from "@/elements/btn";
 import Table from "../../components/table/carTable";
 import Icon from "@/elements/Icon";
 import Cards from "../../components/cardList/carCard";
 import InputText from "@/elements/inputText";
+import CreateCarsModal from "./create/page";
 
 function Cars() {
    const [view, setView] = useState("cards");
    const [searchTerm, setSearchTerm] = useState("");
+   const [showCarModal, setShowCarModal] = useState(false);
 
    return (
       <div>
          <div className="p-2 mb-3 flex flex-row items-center gap-6">
-            <Link href="/cars/create" className="hidden md:flex">
-               <Btn
-                  texto="Novo carro"
-                  variant="primary"
-                  className="gap-3 text-nowrap flex ursor-pointer"
-               >
-                  <Icon name="carPlus" className="size-6" />
-               </Btn>
-            </Link>
+            <Btn
+               texto="Novo carro"
+               variant="primary"
+               onClick={() => setShowCarModal(true)}
+               className="gap-3 text-nowrap ursor-pointer hidden md:flex"
+            >
+               <Icon name="carPlus" className="size-6" />
+            </Btn>
             <div className="w-full md:flex-1">
                <InputText
                   variant="withIcon"
@@ -49,11 +49,14 @@ function Cars() {
             {view === "cards" && <Cards searchTerm={searchTerm} />}
          </div>
 
-         <Link href="/cars/create" className="md:hidden flex">
-            <span className="fixed bottom-0 right-0 m-4 z-50 p-6 bg-bee-purple-600 hover:bg-bee-purple-700 shadow-xl text-white rounded-full transition-colors duration-300">
+         <div className="md:hidden flex">
+            <button  onClick={() => setShowCarModal(true)} className="fixed bottom-0 right-0 m-4 z-50 p-6 bg-bee-purple-600 hover:bg-bee-purple-700 shadow-xl text-white rounded-full transition-colors duration-300">
                <Icon name="carPlus" className="size-6" />
-            </span>
-         </Link>
+            </button>
+         </div>
+         {showCarModal && (
+            <CreateCarsModal onClose={() => setShowCarModal(false)} />
+         )}
       </div>
    );
 }
