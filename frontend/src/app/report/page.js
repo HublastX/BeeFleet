b58d7aa6ea87242d799d5c445f0e720df01ac88a
Andra-sun <camileguimaraes463@gmail.com>
2025-05-25 19:43:00 -0300

    feat(report): formulário de relatório

--- frontend/src/app/report/page.js
+++ frontend/src/app/report/page.js
@@ -1,10 +1,21 @@
-import { Suspense } from "react";
-import ReportContent from "./ReportContent";
-
-export default function RelatoriosPage() {
+import ReportForm from "@/components/reportsForm/mainReport";
+import Icon from "@/elements/Icon";
+export default function Relatorios() {
    return (
-      <Suspense fallback={<div>Carregando relatórios...</div>}>
-         <ReportContent />
-      </Suspense>
+      <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-md">
+         <div className="bg-white dark:bg-bee-dark-800 p-6 rounded-2xl border border-bee-dark-300 dark:border-bee-dark-400 shadow-lg self-center min-w-[350px] max-w-full">
+            <div className="flex items-center gap-3 mb-6">
+               <h2 className="text-2xl font-bold">Gerar Relatorio</h2>
+               <button
+                  className="ml-auto text-gray-400 hover:text-gray-700 dark:hover:text-white text-2xl font-bold focus:outline-none"
+                  aria-label="Fechar"
+                  type="button"
+               >
+                  <Icon name="xMark" className="size-5" strokeWidth={5} />
+               </button>
+            </div>
+            <ReportForm />
+         </div>
+      </div>
    );
 }
