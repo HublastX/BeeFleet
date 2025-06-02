413d09606a97717908a4086b3db1607c330e6a32
wendellast <wendelalves01a@hotmail.com>
2025-06-02 13:10:18 -0300

    feat(routes): implements soft delete and restore routes

--- backend/datahub/routes/private/managers/managersRoutes.ts
+++ backend/datahub/routes/private/managers/managersRoutes.ts
@@ -6,9 +6,16 @@ import { putManager } from "../../../controllers/managers/putManager";
 import { deleteManager } from "../../../controllers/managers/deleteManager";
 import { validate } from "../../../middlewares/validate";
 import { managerSchema } from "../../../schemas/managerInterface";
+import { Request, Response, NextFunction } from "express";
+import { authenticateManager } from "../../../middlewares/auth";
+import {
+    restoreDeletedItem,
+    softDeleteManager,
+} from "../../../controllers/managers/softDeleteController";
 import { createImageUploader } from "./../../../config/storage/storage";
 
 export const uploadManagerImage = createImageUploader("uploads/managers");
+
 const managerRoutes: Router = express.Router();
 
 managerRoutes.post(
@@ -61,5 +68,37 @@ managerRoutes.delete("/managers/:id", async (req, res, next) => {
         next(error);
     }
 });
+
+managerRoutes.patch(
+    "/managers/soft-delete/:id",
+    authenticateManager as (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => void,
+    async (req: Request, res: Response) => {
+        try {
+            await softDeleteManager(req, res);
+        } catch (error) {
+            res.status(500).send({ error: "Internal Server Error" });
+        }
+    }
+);
+
+managerRoutes.patch(
+    "/restore/:itemType/:itemId",
+    authenticateManager as (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => void,
+    async (req: Request, res: Response) => {
+        try {
+            await restoreDeletedItem(req, res);
+        } catch (error) {
+            res.status(500).send({ error: "Internal Server Error" });
+        }
+    }
+);
 
 export default managerRoutes;
