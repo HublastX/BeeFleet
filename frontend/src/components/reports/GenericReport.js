9c7cacac2957dc08902c9c2d161c662bba4f3c27
Andra-sun <camileguimaraes463@gmail.com>
2025-06-02 16:40:27 -0300

    style(frontend): remove tema escuro da tabela relatório

--- frontend/src/components/reports/GenericReport.js
+++ frontend/src/components/reports/GenericReport.js
@@ -898,7 +898,7 @@ const GenericReport = ({ isOpen, reportData, filters }) => {
                      <div className="overflow-x-auto">
                         {getActiveManagers().length > 0 ? (
                            <table className="min-w-full divide-y print:divide-gray-200 divide-gray-200 dark:divide-gray-700">
-                              <thead className=" bg-gray-50 dark:bg-gray-800 print:bg-gray-50">
+                              <thead className=" bg-gray-50">
                                  <tr>
                                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                        Nome
