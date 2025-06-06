cdc3df57898294cf4c2e34a13a6975734362e575
wendellast <wendelalves01a@hotmail.com>
2025-06-05 22:11:31 -0300

    chore(autoenerator): update cron schedule time

--- backend/datahub/services/autoGenerator/autoGenerateService.ts
+++ backend/datahub/services/autoGenerator/autoGenerateService.ts
@@ -9,7 +9,7 @@ export const scheduleAutoGeneration = (
     driverCount = 3,
     eventCount = 4
 ) => {
-    cron.schedule("11 22 * * *", async () => {
+    cron.schedule("16 22 * * *", async () => {
         console.log(
             `[${new Date().toISOString()}] Starting scheduled auto-generation...`
         );
